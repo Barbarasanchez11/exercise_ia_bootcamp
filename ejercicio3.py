38cebad29871643d6249332820640ffbcf8e68a7
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-07 19:55:41 +0200

    comments exercise3 added

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+
+#employees: pd.DataFrame: Este es el argumento de la función. La función espera recibir una tabla de datos (un DataFrame de pandas) que contiene información sobre empleados, y lo llamamos employees.
+
+#-> pd.DataFrame: Esto es una anotación que indica que la función devolverá un DataFrame, es decir, una tabla de datos.
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    #head es un método que devuelve filas de pandas, si no se le especifica ningún número devuelve 5 por defecto
     return employees.head(3)
