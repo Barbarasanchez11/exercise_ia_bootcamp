5e064eb9aae3936a0c5877b0183dcea099b9b5ee
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-07 19:18:52 +0200

    comments addedon exercise 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,4 @@
+#pandas se usa para trabajar con datos en forma de tabla
 import pandas as pd
 
 student_data = [
@@ -7,6 +8,14 @@ student_data = [
   [4, 20]
 ]
 
+#Cuando alguien llame a esta función, deberá pasarle una lista de estudiantes, y esa lista será llamada list_students dentro de la función.list_students es el nombre del parámetro que se usa dentro de la función.
 def createDataframe(list_students):
     df = pd.DataFrame(list_students, columns=["student_id", "age"])
     return df
+
+
+
+df_resultado = createDataframe(student_data)#student_data (la lista real) se pasa como valor al parámetro list_students
+
+#imprimir para verlo en consola
+print(df_resultado) 
